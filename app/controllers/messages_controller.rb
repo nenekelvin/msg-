81b8062e566862db9e01581d420cda6df0711f9b
nenekelvin <karvinmitchar@gmail.com>
2019-08-26 10:30:21 +0300

    added updated the message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
 	def index
 	end
+	def new
+		@messages = Message.new
+	end
+
+	def create
+		@messages =Message.new()
+	end
+	 private
+	 def message_params
+	 	params.require(:message.permit(:title, :description))
+	 end
+
 end
